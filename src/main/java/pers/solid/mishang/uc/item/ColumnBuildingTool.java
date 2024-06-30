1f4f0bf3d977f83230fe5cb784ea839df4acda80
cdpyx <73939862+cdpyx@users.noreply.github.com>
2024-06-30 18:12:53 +0800

    Update ColumnBuildingTool.java

--- src/main/java/pers/solid/mishang/uc/item/ColumnBuildingTool.java
+++ src/main/java/pers/solid/mishang/uc/item/ColumnBuildingTool.java
@@ -1,30 +1,12 @@
 package pers.solid.mishang.uc.item;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.api.EnvironmentInterface;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.ShapeContext;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.data.client.Models;
-import net.minecraft.data.client.TextureKey;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.boss.dragon.EnderDragonPart;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.fluid.FluidState;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
@@ -34,151 +16,119 @@ import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import pers.solid.brrp.v1.generator.ItemResourceGenerator;
-import pers.solid.brrp.v1.model.ModelJsonBuilder;
-import pers.solid.mishang.uc.MishangucClient;
-import pers.solid.mishang.uc.MishangucRules;
-import pers.solid.mishang.uc.mixin.WorldRendererInvoker;
-import pers.solid.mishang.uc.render.RendersBeforeOutline;
-import pers.solid.mishang.uc.util.BlockPlacementContext;
-import pers.solid.mishang.uc.util.TextBridge;
+import net.minecraft.world.event.WorldEvent;
+import net.minecraft.world.fluid.FluidState;
+import net.minecraft.world.render.VertexConsumer;
+import net.minecraft.world.render.VertexConsumerProvider;
+import net.minecraft.world.render.WorldRendererInvoker;
+import net.minecraft.world.render.RenderLayer;
+import net.minecraft.world.render.shape.VoxelShapes;
+import net.minecraft.text.Text;
+import net.minecraft.world.BlockView;
 
+import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
+public class ColumnBuildingTool extends Item {
+    public static boolean suppressOnBlockAdded = false;
 
-
-  @Override
-  public ActionResult useOnBlock(ItemStack stack, PlayerEntity player, World world, BlockHitResult blockHitResult, Hand hand, boolean fluidIncluded) {
-    if (!hasAccess(player, world, true)) {
-      return ActionResult.PASS;
+    public ColumnBuildingTool(Settings settings) {
+        super(settings);
     }
-    BlockPlacementContext blockPlacementContext = new BlockPlacementContext(world, blockHitResult.getBlockPos(), player, player.getStackInHand(hand), blockHitResult, fluidIncluded);
-    blockPlacementContext.playSound();
-    int flags = getFlags(stack);
-    suppressOnBlockAdded = true;
-    blockPlacementContext.setBlockState(flags);
-    suppressOnBlockAdded = false;
-    blockPlacementContext.setBlockEntity();
-    return ActionResult.success(world.isClient);
-  }
 
-  public static boolean suppressOnBlockAdded = false;
-
-  @Override
-  public ActionResult beginAttackBlock(ItemStack stack, PlayerEntity player, World world, Hand hand, BlockPos pos, Direction direction, boolean fluidIncluded) {
-    if (!hasAccess(player, world, true)) {
-      return ActionResult.PASS;
+    public ActionResult useOnBlock(ItemStack stack, PlayerEntity player, World world, BlockHitResult blockHitResult, Hand hand, boolean fluidIncluded) {
+        blockPlacementContext.playSound();
+        int flags = getFlags(stack);
+        suppressOnBlockAdded = true;
+        blockPlacementContext.setBlockState(flags);
+        suppressOnBlockAdded = false;
+        blockPlacementContext.setBlockEntity();
+        return ActionResult.success(world.isClient);
     }
-    final BlockState blockState = world.getBlockState(pos);
-    world.syncWorldEvent(player, 2001, pos, Block.getRawIdFromState(world.getBlockState(pos)));
-    FluidState fluidState = blockState.getFluidState();
-    world.removeBlockEntity(pos);
-    int flags = getFlags(stack);
-    world.setBlockState(pos, fluidIncluded ? Blocks.AIR.getDefaultState() : fluidState.getBlockState(), flags);
-    return ActionResult.success(world.isClient);
-  }
 
-  private static int getFlags(ItemStack stack) {
-    return 0b11010;
-  }
-
-  @Override
-  public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-    super.appendTooltip(stack, world, tooltip, context);
-    tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.1").formatted(Formatting.GRAY));
-    tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.2").formatted(Formatting.GRAY));
-    if (Boolean.TRUE.equals(includesFluid(stack))) {
-      tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.fluids").formatted(Formatting.GRAY));
+    public ActionResult beginAttackBlock(ItemStack stack, PlayerEntity player, World world, Hand hand, BlockPos pos, Direction direction, boolean fluidIncluded) {
+        final BlockState blockState = world.getBlockState(pos);
+        world.syncWorldEvent(player, 2001, pos, Block.getRawIdFromState(world.getBlockState(pos)));
+        FluidState fluidState = blockState.getFluidState();
+        world.removeBlockEntity(pos);
+        int flags = getFlags(stack);
+        world.setBlockState(pos, fluidIncluded ? Blocks.AIR.getDefaultState() : fluidState.getBlockState(), flags);
+        return ActionResult.success(world.isClient);
     }
-    tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.3").formatted(Formatting.GRAY));
-    if ((getFlags(stack) & 128) != 0) {
-      tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.suspends_light").formatted(Formatting.YELLOW));
-    }
-  }
 
-  @Environment(EnvType.CLIENT)
-  @Override
-  public boolean renderBlockOutline(PlayerEntity player, ItemStack itemStack, WorldRenderContext worldRenderContext, WorldRenderContext.BlockOutlineContext blockOutlineContext, Hand hand) {
-    final MinecraftClient client = MinecraftClient.getInstance();
-    if (!hasAccess(player, worldRenderContext.world(), false)) {
-      return true;
-    } else {
-      final Item item = player.getMainHandStack().getItem();
-      if (hand == Hand.OFF_HAND && (item instanceof BlockItem || item instanceof CarryingToolItem)) {
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.1").formatted(Formatting.GRAY));
+        tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.2").formatted(Formatting.GRAY));
+        if (Boolean.TRUE.equals(includesFluid(stack))) {
+            tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.3").formatted(Formatting.GRAY));
+        }
+        if ((getFlags(stack) & 128) != 0) {
+            tooltip.add(TextBridge.translatable("item.mishanguc.force_placing_tool.tooltip.4").formatted(Formatting.GRAY));
+        }
+    }
+
+    public boolean renderBlockOutline(PlayerEntity player, ItemStack itemStack, WorldRenderContext worldRenderContext, WorldRenderContext.BlockOutlineContext blockOutlineContext, Hand hand) {
+        final MinecraftClient client = MinecraftClient.getInstance();
+        if (!hasAccess(player, worldRenderContext.world(), false)) {
+            return false;
+        } else {
+            final Item item = player.getMainHandStack().getItem();
+            if (hand == Hand.OFF_HAND && (item instanceof BlockItem || item instanceof CarryingToolItem)) {
+                return false;
+            }
+        }
+        final VertexConsumerProvider consumers = worldRenderContext.consumers();
+        if (consumers == null) {
+            return false;
+        }
+        final VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.LINES);
+        final BlockHitResult blockHitResult;
+        final MatrixStack matrices = worldRenderContext.matrixStack();
+        HitResult crosshairTarget = client.crosshairTarget;
+        if (crosshairTarget instanceof BlockHitResult) {
+            blockHitResult = (BlockHitResult) crosshairTarget;
+        } else {
+            return false;
+        }
+        final boolean includesFluid = this.includesFluid(itemStack, player.isSneaking());
+        final BlockPlacementContext blockPlacementContext = new BlockPlacementContext(worldRenderContext.world(), blockOutlineContext.blockPos(), player, itemStack, blockHitResult, includesFluid);
+        WorldRendererInvoker.drawCuboidShapeOutline(matrices, vertexConsumer, blockPlacementContext.stateToPlace.getOutlineShape(blockPlacementContext.world, blockPlacementContext.posToPlace, ShapeContext.of(player)), blockPlacementContext.posToPlace.getX() - blockOutlineContext.cameraX(), blockPlacementContext.posToPlace.getY() - blockOutlineContext.cameraY(), blockPlacementContext.posToPlace.getZ() - blockOutlineContext.cameraZ(), 0, 1, 1, 0.8f);
+        if (includesFluid) {
+            WorldRendererInvoker.drawCuboidShapeOutline(matrices, vertexConsumer, fluidState.getOutlineShape(blockPlacementContext.world, blockPlacementContext.posToPlace, ShapeContext.of(player)), blockPlacementContext.posToPlace.getX() - blockOutlineContext.cameraX(), blockPlacementContext.posToPlace.getY() - blockOutlineContext.cameraY(), blockPlacementContext.posToPlace.getZ() - blockOutlineContext.cameraZ(), 0, 1, 1, 0.8f);
+        }
         return true;
-      }
     }
-    final VertexConsumerProvider consumers = worldRenderContext.consumers();
-    if (consumers == null) {
-      return true;
-    }
-    final VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.LINES);
 
-    final BlockHitResult blockHitResult;
-    final MatrixStack matrices = worldRenderContext.matrixStack();
-    HitResult crosshairTarget = client.crosshairTarget;
-    if (crosshairTarget instanceof BlockHitResult) {
-      blockHitResult = (BlockHitResult) crosshairTarget;
-    } else {
-      return true;
+    public @NotNull ActionResult attackEntityCallback(PlayerEntity player, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
+        if (!world.isClient) {
+            if (entity instanceof LivingEntity livingEntity) {
+                livingEntity.damage(DamageSource.player(player), 1.0F);
+            }
+            player.getItemCooldownManager().set(this, 10);
+        }
+        return ActionResult.success(world.isClient);
     }
-    final boolean includesFluid = this.includesFluid(itemStack, player.isSneaking());
-    final BlockPlacementContext blockPlacementContext = new BlockPlacementContext(worldRenderContext.world(), blockOutlineContext.blockPos(), player, itemStack, blockHitResult, includesFluid);
-    WorldRendererInvoker.drawCuboidShapeOutline(matrices, vertexConsumer, blockPlacementContext.stateToPlace.getOutlineShape(blockPlacementContext.world, blockPlacementContext.posToPlace, ShapeContext.of(player)), blockPlacementContext.posToPlace.getX() - blockOutlineContext.cameraX(), blockPlacementContext.posToPlace.getY() - blockOutlineContext.cameraY(), blockPlacementContext.posToPlace.getZ() - blockOutlineContext.cameraZ(), 0, 1, 1, 0.8f);
-    if (includesFluid) {
-      WorldRendererInvoker.drawCuboidShapeOutline(matrices, vertexConsumer, blockPlacementContext.stateToPlace.getFluidState().getShape(blockPlacementContext.world, blockPlacementContext.posToPlace), blockPlacementContext.posToPlace.getX() - blockOutlineContext.cameraX(), blockPlacementContext.posToPlace.getY() - blockOutlineContext.cameraY(), blockPlacementContext.posToPlace.getZ() - blockOutlineContext.cameraZ(), 0, 0.5f, 1, 0.5f);
-    }
-    return false;
-  }
 
-  @Override
-  public @NotNull ActionResult attackEntityCallback(PlayerEntity player, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
-    if (!hasAccess(player, world, true)) return ActionResult.PASS;
-    if (!world.isClient) {
-      if (entity instanceof PlayerEntity) {
-        entity.kill();
-      } else {
-        entity.remove(Entity.RemovalReason.KILLED);
-      }
-      if (entity instanceof EnderDragonPart enderDragonPart) {
-        enderDragonPart.owner.kill();
-      }
+    private static boolean hasAccess(PlayerEntity player, World world, boolean warn) {
+        // Your access logic here
+        return true;
     }
-    return ActionResult.SUCCESS;
-  }
 
-  @ApiStatus.AvailableSince("1.0.0")
-  private static boolean hasAccess(PlayerEntity player, World world, boolean warn) {
-    if (world.isClient) {
-      return MishangucClient.CLIENT_FORCE_PLACING_TOOL_ACCESS.get().hasAccess(player);
-    } else {
-      return true; // Placeholder, replace with actual access check.
+    public void renderBeforeOutline(WorldRenderContext context, HitResult hitResult, ClientPlayerEntity player, Hand hand) {
+        final MatrixStack matrices = context.matrixStack();
+        final VertexConsumerProvider consumers = context.consumers();
+        if (consumers == null) return;
+        final VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.getLines());
+        final Vec3d cameraPos = context.camera().getPos();
+        if (hitResult instanceof EntityHitResult entityHitResult) {
+            final Entity entity = entityHitResult.getEntity();
+            WorldRendererInvoker.drawCuboidShapeOutline(matrices, vertexConsumer, VoxelShapes.cuboid(entity.getBoundingBox()), -cameraPos.x, -cameraPos.y, -cameraPos.z, 1.0f, 0f, 0f, 0.8f);
+        }
     }
-  }
 
-  @Environment(EnvType.CLIENT)
-  @Override
-  public void renderBeforeOutline(WorldRenderContext context, HitResult hitResult, ClientPlayerEntity player, Hand hand) {
-    if (hand != Hand.MAIN_HAND || !hasAccess(player, context.world(), false)) return;
-    final MatrixStack matrices = context.matrixStack();
-    final VertexConsumerProvider consumers = context.consumers();
-    if (consumers == null) return;
-    final VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.getLines());
-    final Vec3d cameraPos = context.camera().getPos();
-    if (hitResult instanceof EntityHitResult entityHitResult) {
-      final Entity entity = entityHitResult.getEntity();
-      WorldRendererInvoker.drawCuboidShapeOutline(matrices, vertexConsumer, VoxelShapes.cuboid(entity.getBoundingBox()), -cameraPos.x, -cameraPos.y, -cameraPos.z, 1.0f, 0f, 0f, 0.8f);
+    public ModelJsonBuilder getItemModel() {
+        // Your model building logic here
+        return new ModelJsonBuilder();
     }
-  }
-
-  @Environment(EnvType.CLIENT)
-  @Override
-  public ModelJsonBuilder getItemModel() {
-    return ModelJsonBuilder.create(Models.HANDHELD).addTexture(TextureKey.LAYER0, getTextureId());
-  }
 }
