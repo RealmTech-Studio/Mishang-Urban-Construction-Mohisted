73eedd477de85efb5957fc87468129ecc25b90c9
cdpyx <73939862+cdpyx@users.noreply.github.com>
2024-06-30 20:17:57 +0800

    Update WorldChunkMixin.java

--- src/main/java/pers/solid/mishang/uc/mixin/WorldChunkMixin.java
+++ src/main/java/pers/solid/mishang/uc/mixin/WorldChunkMixin.java
@@ -7,12 +7,12 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import pers.solid.mishang.uc.item.ForcePlacingToolItem;
+
 
 @Mixin(WorldChunk.class)
 public class WorldChunkMixin {
   @WrapWithCondition(method = "setBlockState", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;onBlockAdded(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V"))
   public boolean wrappedCallOnBlockAdded(BlockState instance, World world, BlockPos pos, BlockState state, boolean notify) {
-    return !ForcePlacingToolItem.suppressOnBlockAdded;
+    return NULL;
   }
 }
